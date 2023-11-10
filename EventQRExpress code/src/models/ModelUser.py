c46c9e05806883b39bdce3657d5f207bcf988b02
Armando Martinez <armando@tecuani.me>
2023-11-09 19:03:29 -0600

    consultas corregidas en ModelUser

--- EventQRExpress code/src/models/ModelUser.py
+++ EventQRExpress code/src/models/ModelUser.py
@@ -6,8 +6,9 @@ class ModelUser():
     def login(self, db, user):
         try:
             cursor = db.connection.cursor()
-            sql = """SELECT id, email, password, nombre FROM usuarios 
-                    WHERE email = '{}'""".format(user.email)
+            # sql = """SELECT id, email, password, nombre FROM usuarios 
+            #         WHERE email = '{}'""".format(user.email)
+            sql = f"SELECT id, email, password, nombre FROM usuarios WHERE email = {user.email}"
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
@@ -23,7 +24,7 @@ class ModelUser():
     def get_by_id(self, db, id):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT id, email, nombre FROM usuarios WHERE id = {}".format(id)
+            sql = f"SELECT id, email, nombre FROM usuarios WHERE id = {id}"
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
@@ -43,8 +44,7 @@ class ModelUser():
             db.connection.commit()
             try:
                 cursor = db.connection.cursor()
-                sql = """SELECT id, email, password, nombre FROM usuarios 
-                        WHERE email = '{}'""".format(user.email)
+                sql = f"SELECT id, email, password, nombre FROM usuarios WHERE email = {user.email}"
                 cursor.execute(sql)
                 row = cursor.fetchone()
                 if row != None:
@@ -61,7 +61,7 @@ class ModelUser():
     def check_email(self, db, email):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT id, nombre FROM usuarios WHERE email = '{}'".format(email)
+            sql = f"SELECT id, nombre FROM usuarios WHERE email = {email}"
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
@@ -75,7 +75,7 @@ class ModelUser():
     def consulta_email(self, db, email):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT id, confirmed, nombre, tipo, confirmed_on FROM usuarios WHERE email = '{}'".format(email)
+            sql = f"SELECT id, confirmed, nombre, tipo, confirmed_on FROM usuarios WHERE email = {email}"
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
@@ -90,7 +90,7 @@ class ModelUser():
     def consulta_telefono(self, db, id):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT telefono FROM usuarios WHERE id =" + str(id)
+            sql = f"SELECT telefono FROM usuarios WHERE id = {id}"
             cursor.execute(sql)
             row = cursor.fetchone()
             if row != None:
@@ -104,7 +104,7 @@ class ModelUser():
     def confirm_user(self, db, email):
         try:
             cursor = db.connection.cursor()
-            sql = "UPDATE usuarios SET confirmed=1, confirmed_on=CURDATE() WHERE email='" + email + "';"
+            sql = f"UPDATE usuarios SET confirmed=1, confirmed_on=CURDATE() WHERE email={email}"
             cursor.execute(sql)
             db.connection.commit()
         except Exception as ex:
@@ -115,7 +115,7 @@ class ModelUser():
     def delete(self, db, id_user):
         try:
             cursor = db.connection.cursor()
-            sql = 'DELETE FROM usuarios WHERE id ='+ id_user
+            sql = f'DELETE FROM usuarios WHERE id ={id_user}'
             cursor.execute(sql)
             db.connection.commit()
         except Exception as ex:
@@ -125,7 +125,8 @@ class ModelUser():
     def update_cliente(self, db, id_recibido, nombre, email, telefono, direccion): #Nota al incrementar la cantidad de locker la disponibilidad cambia, este dato se debe de corregir en el objeto que se envie(diccionario)
         try:
             cursor = db.connection.cursor()
-            sql = 'UPDATE usuarios SET nombre="'+nombre+'", email="'+email+'", telefono="'+telefono+'" WHERE id='+id_recibido
+            # sql = 'UPDATE usuarios SET nombre="'+nombre+'", email="'+email+'", telefono="'+telefono+'" WHERE id='+id_recibido
+            sql = f'UPDATE usuarios SET nombre="{nombre}", email="{email}", telefono="{telefono}" WHERE id={id_recibido}'
             cursor.execute(sql)
             db.connection.commit()
         except Exception as ex:
@@ -136,7 +137,8 @@ class ModelUser():
         try:
             encrypted_password = User.generate_password(contrasena)
             cursor = db.connection.cursor()
-            sql = 'UPDATE usuarios SET password="'+encrypted_password+'" WHERE id='+id_recibido
+            # sql = 'UPDATE usuarios SET password="'+encrypted_password+'" WHERE id='+id_recibido
+            sql = f'UPDATE usuarios SET password="{encrypted_password}" WHERE id={id_recibido}'
             cursor.execute(sql)
             db.connection.commit()
         except Exception as ex:
@@ -146,7 +148,9 @@ class ModelUser():
     def modificar_estado(self, db, id_user, estado):
         try:
             cursor = db.connection.cursor()
-            sql = 'UPDATE usuarios SET confirmed="'+str(estado)+'" WHERE id='+id_user
+            # sql = 'UPDATE usuarios SET confirmed="'+str(estado)+'" WHERE id='+id_user
+            sql = f'UPDATE usuarios SET confirmed="{str(estado)}" WHERE id={id_user}'
+
             cursor.execute(sql)
             db.connection.commit()
         except Exception as ex:
