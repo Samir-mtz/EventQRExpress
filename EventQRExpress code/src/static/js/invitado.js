6bc7ace95ab330356566fda8192b7c3e4e1f5501
BryanHdezL <bryan.hdezl2407@hotmail.com>
2024-01-08 16:45:58 -0600

    Prqueño avance de correcciones frontend

--- EventQRExpress code/src/static/js/invitado.js
+++ EventQRExpress code/src/static/js/invitado.js
@@ -68,9 +68,9 @@ function convertir_HTML_Imagen_Invitacion(id, idEvento, idConfirmacion) {
   var originalHeight = contenedor.offsetHeight;
 
   // Estilos del contenedor
-  contenedor.style.width = "10000px";
-  contenedor.style.height = "650px";
-  contenedor.style.transform = "scale(1.5)";
+  contenedor.style.width = "1300px";
+  contenedor.style.height = "300px";
+  contenedor.style.transform = "scale(1.2)";
 
   // Convertir elemento HTML en PNG
   domtoimage
